2b305cf99d4480ff683247ea7ce5d0d3362d3007
calvinc460 <104769898+calvinc460@users.noreply.github.com>
2022-09-16 21:41:56 -0500

    Added first guess for buttons

--- Test1/test1.py
+++ Test1/test1.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 from tkinter import ttk
 
@@ -9,27 +10,33 @@ def calculate(*args):
         pass
 
 root = Tk()
-root.title("Feet to Meters")
+root.title("Menu Title")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+cMenuFrame = ttk.Frame(root, padding="3 3 12 12")
+cMenuFrame.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+
+btn = list()
+for n in range(1,21) :
+    btn[n] = ttk.Button(cMenuFrame,text=n)
+    btn[n].grid(column=1,row=n+1)
+
 feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
+feet_entry = ttk.Entry(cMenuFrame, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=25, sticky=(W, E))
 
 meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Label(cMenuFrame, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+ttk.Button(cMenuFrame, text="Calculate", command=calculate).grid(column=3, row=27, sticky=W)
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+ttk.Label(cMenuFrame, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(cMenuFrame, text="is equivalent to").grid(column=1, row=28, sticky=E)
+ttk.Label(cMenuFrame, text="meters").grid(column=3, row=28, sticky=W)
 
-for child in mainframe.winfo_children(): 
+for child in cMenuFrame.winfo_children(): 
     child.grid_configure(padx=5, pady=5)
 
 feet_entry.focus()
