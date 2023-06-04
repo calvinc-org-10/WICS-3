d1dae1da63bf7f230b8df40f748c4d338df2d812
Calvin C <curtindolph.calvin.10@gmail.com>
2023-06-03 20:22:04 -0500

    Material Form - mult was not used in the SAP calcs Fixes #14

--- WICS/procs_Material.py
+++ WICS/procs_Material.py
@@ -213,7 +213,7 @@ def fnMaterialForm(req, recNum = -1, gotoRec=False, newRec=False):
 
     # count summary subform
     # SAPTotals = SAP_SOHRecs.objects.all().values('uploaded_at','Material').annotate(SAPQty=Sum('Amount')).order_by('uploaded_at', 'Material')
-    SAPTotals = VIEW_SAP.objects.filter(Material_id=currRec.pk).values('uploaded_at','Material').annotate(SAPQty=Sum('Amount')).order_by('uploaded_at', 'Material')
+    SAPTotals = VIEW_SAP.objects.filter(Material_id=currRec.pk).values('uploaded_at','Material','mult').annotate(SAPQty=Sum('Amount')).order_by('uploaded_at', 'Material')
     raw_countdata = ActualCounts.objects.filter(Material=currRec).order_by('Material','-CountDate').annotate(QtyEval=Value(0, output_field=models.IntegerField()))
     LastMaterial = None ; LastCountDate = None
     initdata = []
@@ -227,11 +227,13 @@ def fnMaterialForm(req, recNum = -1, gotoRec=False, newRec=False):
             LastMaterial = r.Material ; LastCountDate = r.CountDate
             if SAPTotals.filter(uploaded_at__lte=r.CountDate).exists():
                 SAPDate = SAPTotals.filter(uploaded_at__lte=r.CountDate).latest('uploaded_at')['uploaded_at']
-                SAPQty = SAPTotals.filter(uploaded_at__lte=r.CountDate).latest('uploaded_at')['SAPQty']
+                SQ = SAPTotals.filter(uploaded_at__lte=r.CountDate).latest('uploaded_at')
+                SAPQty = SQ['SAPQty'] * SQ['mult']
             else:
                 if SAPTotals.exists():
                     SAPDate = SAPTotals.first()['uploaded_at']
-                    SAPQty = SAPTotals.first()['SAPQty']
+                    SQ = SAPTotals.first()
+                    SAPQty = SQ['SAPQty'] * SQ['mult']
                 else:
                     SAPDate = ''
                     SAPQty = 0
