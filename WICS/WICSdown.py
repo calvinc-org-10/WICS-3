df840f85ed56b6815c721d82ddf4f4ba075e64e0
Calvin C <curtindolph.calvin.10@gmail.com>
2023-07-05 20:47:19 -0500

    should have been included with last commit

--- WICS/WICSdown.py
+++ WICS/WICSdown.py
@@ -2,7 +2,7 @@ import random
 from django.shortcuts import render
 
 def WICSdown(req):
-        randsvg = random.randint(1,4)
+        randsvg = random.randint(1,5)
         randsvg = f'{randsvg:02}'
 
         templt = "UnderMaintenance.html"
