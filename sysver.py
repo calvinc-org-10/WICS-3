86836d4b4017e67b0083e6bcd9efb50463389c3d
Calvin C <curtindolph.calvin.10@gmail.com>
2023-07-20 19:28:35 -0500

    change version number

--- sysver.py
+++ sysver.py
@@ -1,6 +1,6 @@
 _base_ver_major=3
 _base_ver_minor=1
-_base_ver_patch=1
+_base_ver_patch=2
 _base_ver = str(_base_ver_major) +'.'+ str(_base_ver_minor) +'.'+ str(_base_ver_patch)
 sysver = {
     'DEV': 'DEV'+_base_ver, 
